083b3911743b4c9ffa8eb51d41701b549b04d18a
Andrew Babichev <andrew.babichev@gmail.com>
2019-07-21 14:47:29 +0300

    Disable prawn warning about limited UTF-8 support for built-in fonts

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,13 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'full_time'
+require 'prawn'
 
 RSpec.configure do |config|
   config.alias_it_behaves_like_to :it_embraces, 'embraces'
 end
+
+# PDF 's built-in fonts have very limited support for internationalized text.
+# If you need full UTF-8 support, consider using a TTF font instead.
+#
+# To disable this warning, add the following line to your code:
+Prawn::Font::AFM.hide_m17n_warning = true
